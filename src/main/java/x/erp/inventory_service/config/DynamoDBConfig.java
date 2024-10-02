df149d3b1bee78ce28edf6cd216fc83d13ae1d5a
Manoj D More <manoj.contactpoint@gmail.com>
2024-10-02 15:52:28 +0530

    logout changes & aws secretKey removed

--- src/main/java/x/erp/inventory_service/config/DynamoDBConfig.java
+++ src/main/java/x/erp/inventory_service/config/DynamoDBConfig.java
@@ -4,25 +4,20 @@ package x.erp.inventory_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
 
 @Configuration
 public class DynamoDBConfig {
 
-    @Value("${aws.accessKeyId}")
-    private String awsAccessKey;
-
-    @Value("${aws.secretKey}")
-    private String awsSecretKey;
+//    @Value("${aws.accessKeyId}")
+//    private String awsAccessKey;
+//
+//    @Value("${aws.secretKey}")
+//    private String awsSecretKey;
 
     @Value("${aws.region}")
     private String awsRegion;
--- src/main/java/x/erp/inventory_service/controller/CommonController.java
+++ src/main/java/x/erp/inventory_service/controller/CommonController.java
@@ -1,5 +1,9 @@
 package x.erp.inventory_service.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +13,12 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/erp")
 public class CommonController {
 
+    private static final Logger log = LoggerFactory.getLogger(CommonController.class);
+
     @GetMapping("/dashboard")
-    public String  dashboard() {
+    public String  dashboard(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Authorization: {}", request.getHeader("Authorization"));
+        response.setHeader("Authorization", request.getHeader("Authorization"));
         return "/page/dashboard.html";
     }
 }
